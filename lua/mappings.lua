012eeea25245975a93d311ac7a898c0b05106267
Roee Shapira <roy.shap1@gmail.com>
2021-05-08 23:06:26 +0300

    Added lua lsp.

---
+++ lua/mappings.lua
@@ -0,0 +1,1 @@
+vimp.nnoremap('<leader>1', [[:vsplit ~/.config/nvim/init.vim<cr>]])
---
+++ lua/utils/init.lua
@@ -0,0 +1,17 @@
+local M = {}
+
+function M.trim(s)
+	return s:match '^%s*(.-)%s*$'
+end
+
+function M.system_name()
+	if vim.fn.has('mac') == 1 then
+		return 'macOS'
+	elseif vim.fn.has('unix') == 1 then
+		return 'Linux'
+	elseif vim.fn.has('win32') == 1 then
+		return 'Windows'
+	end
+end
+
+return M

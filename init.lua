a489163739e259280373dbd9b325a2af48ae086e
Roee Shapira <ro33.sha@gmail.com>
2021-05-08 23:37:27 +0300

    Added augroups.

--- init.lua
+++ init.lua
@@ -6,8 +6,8 @@ o.shiftwidth = o.tabstop
 g.mapleader = ' '
 g.maplocalleader = ','
 
-require'packer'.bootstrap()
-require'packer'.setup()
+require'plugins'.bootstrap()
+require'plugins'.setup()
 
 require'colors'
 require'treesitter'
@@ -22,3 +22,19 @@ require'lsp/lua'
 local utils = require'utils'
 
 g.python3_host_prog = utils.trim(vim.fn.system('which python3'))
+
+-- Trigger autoread when changing buffers inside while inside vim
+vim.cmd([[
+augroup autoread_on_buffer_change
+	autocmd!
+	autocmd FocusGained,BufEnter * :checktime
+augroup END
+]])
+
+-- Return to the last editing point when opening files
+vim.cmd([[
+augroup last_read_point
+	autocmd!
+	autocmd BufReadPost * if line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g'\"" | endif
+augroup END
+]])
--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,1 +1,3 @@
 vimp.nnoremap('<leader>1', [[:vsplit ~/.config/nvim/init.vim<cr>]])
+
+vimp.vnoremap('<C-c>', '"+y')

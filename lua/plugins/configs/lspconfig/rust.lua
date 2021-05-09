e022ec359098d38310146a14eedfacca4e48c430
Roee Shapira <roy.shap1@gmail.com>
2021-05-09 13:47:32 +0300

    Added rust lsp config.

---
+++ lua/plugins/configs/lspconfig/rust.lua
@@ -0,0 +1,8 @@
+require'lspconfig'.rust_analyzer.setup {
+	cmd = { "rust-analyzer" }
+    filetypes = { "rust" }
+    root_dir = root_pattern("Cargo.toml", "rust-project.json")
+    settings = {
+    	["rust-analyzer"] = {}
+    }
+}

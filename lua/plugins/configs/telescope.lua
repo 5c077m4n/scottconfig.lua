c6b4d2357e9a009992176b48a540589e7eda0b37
Roee Shapira <roy.shap1@gmail.com>
2021-05-09 23:30:43 +0300

    Improved live grep.

--- lua/plugins/configs/telescope.lua
+++ lua/plugins/configs/telescope.lua
@@ -1,5 +1,7 @@
 local telescope_builtin = require'telescope.builtin'
 
 vimp.nnoremap('<leader>ffl', telescope_builtin.find_files)
-vimp.nnoremap('<leader>fsr', telescope_builtin.live_grep)
+vimp.nnoremap('<leader>fsr', function()
+	telescope_builtin.live_grep { search_dir = {vim.fn.expand('%:p')} }
+end)
 vimp.nnoremap('<leader>fb', telescope_builtin.buffers)

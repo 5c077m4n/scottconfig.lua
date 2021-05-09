b37a61d56d1bcab6aa47d6c181c582e4a235e490
Roee Shapira <ro33.sha@gmail.com>
2021-05-09 16:11:44 +0300

    Added commands.

--- lua/mappings/init.lua
+++ lua/mappings/init.lua
@@ -1,6 +1,7 @@
 vimp.nnoremap('<leader>1', [[:edit ~/.config/nvim/init.lua<CR>]])
 vimp.nnoremap('<leader>2', [[:luafile ~/.config/nvim/init.lua<CR>]])
 
+-- Splits
 vimp.nnoremap('<C-h>', '<C-w>h')
 vimp.nnoremap('<C-j>', '<C-w>j')
 vimp.nnoremap('<C-k>', '<C-w>k')
@@ -10,6 +11,7 @@ vimp.nnoremap({'silent'}, '<leader>wv', ':vertical split<CR>')
 vimp.nnoremap({'silent'}, '<leader>wh', ':split<CR>')
 vimp.nnoremap({'silent'}, '<leader>wt', ':vs#<CR>')
 
+-- Tabs
 vimp.nnoremap({'silent'}, '<leader>tn', ':tab split<CR>')
 vimp.nnoremap({'silent'}, '<leader>tq', ':tabclose<CR>')
 vimp.nnoremap({'silent'}, '<leader>tQ', ':tabonly<CR>')
@@ -17,8 +19,9 @@ vimp.nnoremap({'silent'}, '<leader>]', ':tabn<CR>')
 vimp.nnoremap({'silent'}, '<leader>[', ':tabp<CR>')
 vimp.nnoremap({'silent'}, '<leader>tl', ':tabs<CR>')
 
+-- Ctrl+c copys to clipboard
 vimp.vnoremap('<C-c>', '"+y')
-
+-- Join line does not go one down
 vimp.nnoremap('J', 'mzJ`z')
 -- Remap VIM 0 to first non-blank character
 vimp.nnoremap('0', '^')
@@ -26,6 +29,15 @@ vimp.vnoremap('0', '^')
 
 -- Unsets the last search pattern register by hitting return
 vimp.nnoremap('<CR>', ':noh<CR>')
+
+-- Word traversing
+vimp.inoremap('<C-b>', '<C-o>b')
+vimp.inoremap('<C-e>', '<C-o>e')
+vimp.inoremap('<C-w>', '<C-o>w')
+vimp.inoremap('<C-h>', '<C-o>h')
+vimp.inoremap('<C-j>', '<C-o>j')
+vimp.inoremap('<C-k>', '<C-o>k')
+vimp.inoremap('<C-l>', '<C-o>l')
 
 -- Switch CWD to the directory of the open buffer
 vimp.nnoremap('<leader>cd', ':cd %:p:h<CR>:pwd<CR>')

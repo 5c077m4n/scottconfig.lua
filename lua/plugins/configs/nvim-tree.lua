a0167e2e6e7b8537d3a83470bef07610ddc4669f
Roee Shapira <ro33.sha@gmail.com>
2021-05-09 20:45:50 +0300

    Added lua themes.

--- lua/plugins/configs/nvim-tree.lua
+++ lua/plugins/configs/nvim-tree.lua
@@ -1,14 +1,15 @@
 local g = vim.g
 
 g.nvim_tree_side = "left"
-g.nvim_tree_width = 25
-g.nvim_tree_auto_open = 0
+g.nvim_tree_width = 40
+g.nvim_tree_auto_open = 1
 g.nvim_tree_auto_close = 0
 g.nvim_tree_quit_on_open = 0
 g.nvim_tree_follow = 1
 g.nvim_tree_indent_markers = 1
-g.nvim_tree_hide_dotfiles = 1
+g.nvim_tree_hide_dotfiles = 0
 g.nvim_tree_git_hl = 1
 g.nvim_tree_root_folder_modifier = ":~"
 g.nvim_tree_tab_open = 1
-g.nvim_tree_allow_resize = 1
+g.nvim_tree_allow_resize = 0
+g.nvim_tree_add_trailing = 1

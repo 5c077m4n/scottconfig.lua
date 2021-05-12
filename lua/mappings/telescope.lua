b4d538109bff40b8eb3a469a65ff589ee633863d
Roee Shapira <ro33.sha@gmail.com>
2021-05-12 13:03:14 +0300

    Used silver searcher for telescope.

--- lua/mappings/telescope.lua
+++ lua/mappings/telescope.lua
@@ -1,7 +1,7 @@
 local telescope_builtin = require'telescope.builtin'
 
 vimp.nnoremap('<leader>ffl', telescope_builtin.find_files)
-vimp.nnoremap('<leader>fsr', function()
+vimp.nnoremap('<leader>fs', function()
 	telescope_builtin.live_grep { search_dir = {vim.fn.getcwd()} }
 end)
 vimp.nnoremap('<leader>fb', telescope_builtin.buffers)

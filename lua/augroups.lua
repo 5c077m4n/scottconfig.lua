b4c6a416927aa4cc7255954ed7aa2037a2e214ed
Roee Shapira <roy.shap1@gmail.com>
2021-05-13 11:41:09 +0300

    Fixed aurgoups syntax errors.

--- lua/augroups.lua
+++ lua/augroups.lua
@@ -1,23 +1,29 @@
 local cmd = vim.cmd
 
 -- Re-read file when re-entering it
-pcall(cmd [[
-augroup autoread_on_buffer_change
+pcall(function() 
+	cmd [[
+	augroup autoread_on_buffer_change
 	autocmd!
 	autocmd FocusGained,BufEnter * :checktime
-augroup END
-]])
+	augroup END
+	]]
+end)
 
-pcall(cmd [[
-augroup last_read_point_on_file_open
+pcall(function()
+	cmd [[
+	augroup last_read_point_on_file_open
 	autocmd!
 	autocmd BufReadPost * if line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g'\"" | endif
-augroup END
-]])
+	augroup END
+	]]
+end)
 
-pcall(cmd [[
-augroup highlight_yank
-    autocmd!
-    autocmd TextYankPost * silent! lua require'vim.highlight'.on_yank("IncSearch", 1000)
-augroup END
-]])
+pcall(function()
+	cmd [[
+	augroup highlight_yank
+	autocmd!
+	autocmd TextYankPost * silent! lua require'vim.highlight'.on_yank("IncSearch", 1000)
+	augroup END
+	]]
+end)

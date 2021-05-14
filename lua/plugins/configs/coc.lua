bb30ae6ba88075a15e2a1b9e5743eae0c68b665f
Roee Shapira <roy.shap1@gmail.com>
2021-05-14 12:34:45 +0300

    Minor refactor

--- lua/plugins/configs/coc.lua
+++ lua/plugins/configs/coc.lua
@@ -1,11 +1,9 @@
 local coc_utils = require'utils.coc'
 
-local cmd = vim.api.nvim_command
-
 vim.g.coc_global_extensions = coc_utils.get_coc_extenstions()
 
 pcall(function()
-	cmd [[
+	vim.api.nvim_command [[
 	augroup highlight_on_hover
 	autocmd!
 	autocmd CursorHold * silent call CocActionAsync('highlight')

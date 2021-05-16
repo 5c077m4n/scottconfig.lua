8d453865300a365cfcd99e37246105793f869d3b
Roee Shapira <roy.shap1@gmail.com>
2021-05-16 13:51:28 +0300

    Added custom remove namespace function.

--- lua/vimrc/mappings/init.lua
+++ lua/vimrc/mappings/init.lua
@@ -1,7 +1,7 @@
 vimp.nnoremap('<leader>1', [[:edit ~/.config/nvim/init.lua<CR>]])
 vimp.nnoremap('<leader>2', function()
 	vimp.unmap_all()
-	require'plenary.reload'.reload_modules('vimrc')
+	require'utils.modules'.unload_lua_namespace('vimrc')
 	require'vimrc'
 end)
 vimp.nnoremap('<leader>3', [[:PackerInstall<CR>]])
---
+++ lua/vimrc/utils/modules.lua
@@ -0,0 +1,13 @@
+local M = {}
+
+function M.unload_lua_namespace()
+	local prefix_with_dot = prefix .. '.'
+
+	for key, value in pairs(package.loaded) do
+		if key == prefix or key:sub(1, #prefix_with_dot) == prefix_with_dot then
+			package.loaded[key] = nil
+		end
+	end
+end
+
+return M

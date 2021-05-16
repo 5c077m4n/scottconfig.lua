64d92f4eab99e0e459c090d256552da8b842d354
Roee Shapira <ro33.sha@gmail.com>
2021-05-16 14:03:14 +0300

    Typo.

--- lua/vimrc/utils/modules.lua
+++ lua/vimrc/utils/modules.lua
@@ -5,7 +5,7 @@ function M.unload_lua_namespace(prefix)
 
 	for module_name, _ in pairs(package.loaded) do
 		if module_name == prefix or string.match(module_name, '^' .. prefix_with_dot) then
-			package.loaded[key] = nil
+			package.loaded[module_name] = nil
 		end
 	end
 end

99609fff43016d2d64df284383f876e029e5d377
Roee Shapira <roy.shap1@gmail.com>
2021-05-17 09:22:27 +0300

    Added telescope commands

--- lua/vimrc/plugins/telescope/commands.lua
+++ lua/vimrc/plugins/telescope/commands.lua
@@ -4,4 +4,7 @@ vimp.nnoremap('<leader>ffl', telescope_builtin.find_files)
 vimp.nnoremap('<leader>fs', function()
 	telescope_builtin.live_grep { search_dir = {vim.fn.getcwd()} }
 end)
+vimp.nnoremap('<leader>ffs', telescope_builtin.current_buffer_fuzzy_find)
 vimp.nnoremap('<leader>fb', telescope_builtin.buffers)
+vimp.nnoremap('<leader>fc', telescope_builtin.git_commits)
+vimp.nnoremap('<leader>ffc', telescope_builtin.git_bcommits)

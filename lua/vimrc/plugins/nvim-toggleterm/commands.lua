4425e9becc95737d78296e43742c1573b2da2a17
Roee Shapira <roy.shap1@gmail.com>
2021-05-18 23:43:35 +0300

    Removed old commands.

--- lua/vimrc/plugins/nvim-toggleterm/commands.lua
+++
@@ -1,8 +0,0 @@
-vimp.nnoremap({'silent'}, '<F9>', ':FloatermPrev<CR>')
-vimp.tnoremap({'silent'}, '<F9>', [[<C-\><C-n>:FloatermPrev<CR>]])
-vimp.nnoremap({'silent'}, '<F10>', ':FloatermNext<CR>')
-vimp.tnoremap({'silent'}, '<F10>', [[<C-\><C-n>:FloatermNext<CR>]])
-vimp.nnoremap({'silent'}, '<F11>', ':FloatermNew<CR>')
-vimp.tnoremap({'silent'}, '<F11>', [[<C-\><C-n>:FloatermNew<CR>]])
-vimp.nnoremap({'silent'}, '<F12>', ':FloatermToggle<CR>')
-vimp.tnoremap({'silent'}, '<F12>', [[<C-\><C-n>:FloatermToggle<CR>]])

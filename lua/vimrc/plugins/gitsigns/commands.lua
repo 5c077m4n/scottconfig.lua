2d27f0dcc35a0446f609e2db197536b06092206d
Roee Shapira <roy.shap1@gmail.com>
2021-05-20 11:06:14 +0300

    Added gitsigns commands

---
+++ lua/vimrc/plugins/gitsigns/commands.lua
@@ -0,0 +1,6 @@
+local gitsigns = require'gitsigns'
+
+vimp.nnoremap('<leader>hr', gitsigns.reset_hunk)
+vimp.nnoremap('<leader>hR', gitsigns.reset_buffer)
+vimp.nnoremap('<leader>hp', gitsigns.preview_hunk)
+vimp.nnoremap('<leader>hb', function() gitsigns.preview_hunk(true) end)

572a2cec0dd2e3114998fd0163a909b0a63f110a
Roee Shapira <ro33.sha@gmail.com>
2021-05-23 22:47:35 +0300

    Added which key register

--- lua/vimrc/plugins/telescope/commands.lua
+++ lua/vimrc/plugins/telescope/commands.lua
@@ -1,10 +1,27 @@
 local telescope_builtin = require'telescope.builtin'
 
-vimp.nnoremap('<leader>ffl', telescope_builtin.find_files)
-vimp.nnoremap('<leader>ffg', telescope_builtin.git_files)
+vimp.nnoremap('<leader>fls', telescope_builtin.find_files)
+vimp.nnoremap('<leader>fgl', telescope_builtin.git_files)
 vimp.nnoremap('<leader>fs', telescope_builtin.live_grep)
-vimp.nnoremap('<leader>ffs', telescope_builtin.current_buffer_fuzzy_find)
 vimp.nnoremap('<leader>fb', telescope_builtin.buffers)
 vimp.nnoremap('<leader>fo', telescope_builtin.oldfiles)
 vimp.nnoremap('<leader>fc', telescope_builtin.git_commits)
 vimp.nnoremap('<leader>ffc', telescope_builtin.git_bcommits)
+vimp.nnoremap('<leader>ffs', telescope_builtin.current_buffer_fuzzy_find)
+
+require'which-key'.register {
+	['<leader>f'] = {
+		name = 'Fuzzy search',
+		f = {
+			name = '+current file search',
+			c = '+commit list',
+			s = '+fuzzy find',
+		},
+		ls = 'File list',
+		gl = 'Git file list',
+		s = 'Live grep',
+		b = 'Buffer list',
+		o = 'Old files',
+		c = 'Git commits',
+	},
+}

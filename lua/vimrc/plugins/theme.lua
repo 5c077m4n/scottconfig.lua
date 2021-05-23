c0a6dd918e0039c9cdf1c5798490db497cc5c72d
Roee Shapira <ro33.sha@gmail.com>
2021-05-23 21:54:44 +0300

    Added bufferline config

--- lua/vimrc/plugins/theme.lua
+++ lua/vimrc/plugins/theme.lua
@@ -3,5 +3,11 @@ vim.g.material_style = 'darker'
 
 require'material'.set()
 require'colorizer'.setup()
-require'bufferline'.setup()
+require'bufferline'.setup {
+	separator_style = 'slant',
+	diagnostics = 'nvim_lsp',
+	sort_by = function(buffer_a, buffer_b)
+		return buffer_a.modified > buffer_b.modified
+	end,
+}
 require'todo-comments'.setup()

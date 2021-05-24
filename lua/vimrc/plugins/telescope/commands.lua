60d9eb7f024ee8bc456c85bf4576b33658cc139c
Roee Shapira <roy.shap1@gmail.com>
2021-05-24 22:58:05 +0300

    Added telescope commands

--- lua/vimrc/plugins/telescope/commands.lua
+++ lua/vimrc/plugins/telescope/commands.lua
@@ -4,10 +4,12 @@ vimp.nnoremap('<leader>fls', telescope_builtin.find_files)
 vimp.nnoremap('<leader>fgs', telescope_builtin.git_files)
 vimp.nnoremap('<leader>fs', telescope_builtin.live_grep)
 vimp.nnoremap('<leader>fb', telescope_builtin.buffers)
+vimp.nnoremap('<leader>fm', telescope_builtin.marks)
 vimp.nnoremap('<leader>fo', telescope_builtin.oldfiles)
 vimp.nnoremap('<leader>fc', telescope_builtin.git_commits)
 vimp.nnoremap('<leader>ffc', telescope_builtin.git_bcommits)
 vimp.nnoremap('<leader>ffs', telescope_builtin.current_buffer_fuzzy_find)
+vimp.nnoremap('<leader>fft', telescope_builtin.filetypes)
 
 require'which-key'.register {
 	['<leader>f'] = {
@@ -16,6 +18,7 @@ require'which-key'.register {
 			name = '+current file search',
 			c = '+commit list',
 			s = '+fuzzy find',
+			t = '+filetypes',
 		},
 		ls = 'File list',
 		gs = 'Git file list',
@@ -23,5 +26,6 @@ require'which-key'.register {
 		b = 'Buffer list',
 		o = 'Old files',
 		c = 'Git commits',
+		m = 'Bookmarks',
 	},
 }

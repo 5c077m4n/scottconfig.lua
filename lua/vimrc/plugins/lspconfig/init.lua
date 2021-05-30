a923042fdd8a6693107e4b0cf9a5b09d79f1ebf1
Roee Shapira <roy.shap1@gmail.com>
2021-05-30 12:17:30 +0300

    Added lspsaga

--- lua/vimrc/plugins/lspconfig/init.lua
+++ lua/vimrc/plugins/lspconfig/init.lua
@@ -1,6 +1,19 @@
 local nvim_lsp = require'lspconfig'
 local lspinstall = require'lspinstall'
 local lsp_sig = require'lsp_signature'
+local saga = require'lspsaga'
+
+saga.init_lsp_saga {
+	code_action_keys = {
+		quit = 'q',
+		exec = '<CR>'
+	},
+	rename_action_keys = {
+		quit = {'<C-c>', '<Esc>'},
+		exec = '<CR>'
+	},
+	rename_prompt_prefix = '>',
+}
 
 local function on_attach(_client, bufnr)
 	local lsp = vim.lsp
@@ -16,15 +29,22 @@ local function on_attach(_client, bufnr)
 	vimp.add_buffer_maps(bufnr, function()
 		vimp.nnoremap({'silent'}, 'gD', lsp.buf.declaration)
 		vimp.nnoremap({'silent'}, 'gd', lsp.buf.definition)
-		vimp.nnoremap({'silent'}, 'K', lsp.buf.hover)
+		vimp.nnoremap({'silent'}, 'K', require('lspsaga.hover').render_hover_doc)
+		vimp.nnoremap({'silent'}, '<C-f>', function() 
+			require'lspsaga.action'.smart_scroll_with_saga(1)
+		end)
+		vimp.nnoremap({'silent'}, '<C-b>', function() 
+			require'lspsaga.action'.smart_scroll_with_saga(-1)
+		end)
 		vimp.nnoremap({'silent'}, 'gi', lsp.buf.implementation)
 		vimp.nnoremap({'silent'}, '<leader>gD', lsp.buf.type_definition)
-		vimp.nnoremap({'silent'}, '<leader>rn', lsp.buf.rename)
+		vimp.nnoremap({'silent'}, '<leader>rn', require'lspsaga.rename'.rename)
 		vimp.nnoremap({'silent'}, 'gr', lsp.buf.references)
-		vimp.nnoremap({'silent'}, '<leader>ca', lsp.buf.code_action)
 		vimp.nnoremap({'silent'}, 'g[', lsp.diagnostic.goto_prev)
 		vimp.nnoremap({'silent'}, 'g]', lsp.diagnostic.goto_next)
 		vimp.nnoremap({'silent'}, '<leader>l', lsp.buf.formatting)
+		vimp.nnoremap({'silent'}, '<leader>ca', require'lspsaga.codeaction'.code_action)
+		vimp.vnoremap({'silent'}, '<leader>ca', require'lspsaga.codeaction'.range_code_action)
 	end)
 end
 

4b7373562d247e4129969d35fed7506f022df695
Roee Shapira <roy.shap1@gmail.com>
2021-06-01 11:39:13 +0300

    Removed run_in_modifiable_only uses

--- lua/vimrc/plugins/terminal/commands.lua
+++ lua/vimrc/plugins/terminal/commands.lua
@@ -1,10 +1,8 @@
-local run_in_modifiable_only = require'vimrc.utils'.run_in_modifiable_only
-
-vimp.nnoremap({'silent'}, '<F9>', run_in_modifiable_only(':FloatermPrev<CR>'))
+vimp.nnoremap({'silent'}, '<F9>', ':FloatermPrev<CR>')
 vimp.tnoremap({'silent'}, '<F9>', [[<C-\><C-n>:FloatermPrev<CR>]])
-vimp.nnoremap({'silent'}, '<F10>', run_in_modifiable_only(':FloatermNext<CR>'))
+vimp.nnoremap({'silent'}, '<F10>', ':FloatermNext<CR>')
 vimp.tnoremap({'silent'}, '<F10>', [[<C-\><C-n>:FloatermNext<CR>]])
-vimp.nnoremap({'silent'}, '<F11>', run_in_modifiable_only(':FloatermNew<CR>'))
+vimp.nnoremap({'silent'}, '<F11>', ':FloatermNew<CR>')
 vimp.tnoremap({'silent'}, '<F11>', [[<C-\><C-n>:FloatermNew<CR>]])
-vimp.nnoremap({'silent'}, '<F12>', run_in_modifiable_only(':FloatermToggle<CR>'))
+vimp.nnoremap({'silent'}, '<F12>', ':FloatermToggle<CR>')
 vimp.tnoremap({'silent'}, '<F12>', [[<C-\><C-n>:FloatermToggle<CR>]])

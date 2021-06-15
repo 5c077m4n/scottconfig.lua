25596d103617f33a0b2aed942df55903cc6f5460
Roee Shapira <roy.shap1@gmail.com>
2021-06-15 12:07:43 +0300

    Used tokyonight theme

--- lua/vimrc/plugins/init.lua
+++ lua/vimrc/plugins/init.lua
@@ -20,7 +20,7 @@ local function init_packer()
 			use 'svermeulen/vimpeccable'
 			use 'nvim-lua/plenary.nvim'
 			-- Theme
-			use 'tanvirtin/monokai.nvim'
+			use 'folke/tokyonight.nvim'
 			use 'norcalli/nvim-colorizer.lua'
 			use 'folke/todo-comments.nvim'
 			use {

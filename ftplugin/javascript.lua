4b4cb6f4246e6861c518178fbdca3ead94e0748e
Roee Shapira <roy.shap1@gmail.com>
2021-06-15 12:21:18 +0300

    Added dap ui

--- ftplugin/javascript.lua
+++ ftplugin/javascript.lua
@@ -1,4 +1,6 @@
 vim.cmd [[packadd nvim-dap]]
+vim.cmd [[packadd nvim-dap-ui]]
 vim.cmd [[packadd DAPInstall.nvim]]
 
 require'dap-install'.config('jsnode_dbg', {})
+require'dapui'.setup()
--- ftplugin/rust.lua
+++ ftplugin/rust.lua
@@ -1,4 +1,6 @@
 vim.cmd [[packadd nvim-dap]]
+vim.cmd [[packadd nvim-dap-ui]]
 vim.cmd [[packadd DAPInstall.nvim]]
 
 require'dap-install'.config('ccppr_lldb_dbg', {})
+require'dapui'.setup()

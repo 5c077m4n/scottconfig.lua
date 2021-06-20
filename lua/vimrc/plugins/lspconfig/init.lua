2f28337787f24446d68110760b8828f4df6ebc24
Roee Shapira <roy.shap1@gmail.com>
2021-06-20 13:33:34 +0300

    Updated format command

--- lua/vimrc/plugins/lspconfig/init.lua
+++ lua/vimrc/plugins/lspconfig/init.lua
@@ -68,8 +68,8 @@ local function on_attach(client, bufnr)
 		vimp.nnoremap({'silent', 'override'}, '<leader>ca', require'lspsaga.codeaction'.code_action)
 		vimp.vnoremap({'silent', 'override'}, '<leader>ca', require'lspsaga.codeaction'.range_code_action)
 
-		vimp.nnoremap({'silent', 'override'}, '<leader>l', ':Neoformat<CR>')
-		vimp.vnoremap({'silent', 'override'}, '<leader>l', ':Neoformat! ' .. vim.bo.filetype .. '<CR>')
+		vimp.nnoremap({'silent', 'override'}, '<leader>l', ':Neoformat | :e!<CR>')
+		vimp.vnoremap({'silent', 'override'}, '<leader>l', ':Neoformat! ' .. vim.bo.filetype .. ' | :e!<CR>')
 	end)
 
 	which_key.register({

19ff3f25515d671ae60dfc000b4a6299d9bd95ac
Roee Shapira <ro33.sha@gmail.com>
2021-06-22 13:26:24 +0300

    Minor refactor

--- lua/vimrc/utils/init.lua
+++ lua/vimrc/utils/init.lua
@@ -28,16 +28,6 @@ function M.system_name()
 	end
 end
 
-function M.unload_lua_namespace(prefix)
-	local prefix_with_dot = prefix .. '.'
-
-	for key, value in pairs(package.loaded) do
-		if key == prefix or key:sub(1, #prefix_with_dot) == prefix_with_dot then
-			package.loaded[key] = nil
-		end
-	end
-end
-
 function M.check_back_space()
 	local col = vim.fn.col('.') - 1
 

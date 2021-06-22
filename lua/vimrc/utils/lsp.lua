4e45ab449b7eca83fde6647aa195c33ce2ed00f0
Roee Shapira <ro33.sha@gmail.com>
2021-06-22 12:57:15 +0300

    Added lsp util function

---
+++ lua/vimrc/utils/lsp.lua
@@ -0,0 +1,18 @@
+local M = {}
+
+local function M.lsp_progress()
+	local messages = vim.lsp.util.get_progress_messages()
+	if #messages == 0 then return end
+
+	local status = {}
+	for _, msg in pairs(messages) do
+		table.insert(status, (msg.percentage or 0) .. "%% " .. (msg.title or ""))
+	end
+
+	local spinners = { "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏" }
+	local ms = vim.loop.hrtime() / 1000000
+	local frame = math.floor(ms / 120) % #spinners
+	return table.concat(status, " | ") .. " " .. spinners[frame + 1]
+end
+
+return M

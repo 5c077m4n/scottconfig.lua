cc9095f016b033715d6f7335fb7c1b5690969020
Roee Shapira <ro33.sha@gmail.com>
2021-06-24 15:55:40 +0300

    Moved git commands

--- lua/vimrc/plugins/git/commands.lua
+++ lua/vimrc/plugins/git/commands.lua
@@ -4,3 +4,11 @@ vimp.nnoremap('<leader>hr', gitsigns.reset_hunk)
 vimp.nnoremap('<leader>hR', gitsigns.reset_buffer)
 vimp.nnoremap('<leader>hp', gitsigns.preview_hunk)
 vimp.nnoremap('<leader>hb', function() gitsigns.preview_hunk(true) end)
+
+-- Git mergetool
+vimp.nnoremap('<leader>gs', [[:Gstatus<CR>]])
+vimp.nnoremap('<leader>gm', [[:G mergetool<CR>]])
+vimp.nnoremap('<leader>gdd', [[:Gvdiffsplit!<CR>]])
+vimp.nnoremap('<leader>gdh', [[:diffget //2<CR>]])
+vimp.nnoremap('<leader>gdl', [[:diffget //3<CR>]])
+vimp.nnoremap('<leader>gdD', [[:diffoff<CR>]])

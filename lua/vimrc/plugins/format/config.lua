6f530b37c3c1e2efcb5e4f5fdc31f26f6ae63630
Roee Shapira <ro33.sha@gmail.com>
2021-07-15 12:46:45 +0300

    Added formatter config dir

--- lua/vimrc/plugins/format/config.lua
+++ lua/vimrc/plugins/format/config.lua
@@ -6,11 +6,9 @@ require'format'.setup {
 	vimwiki = {{cmd = {'prettier -w --parser babel'}, start_pattern = '^{{{javascript$', end_pattern = '^}}}$'}},
 	lua = {{cmd = {function(file) return string.format('lua-format', file) end}}},
 	go = {{cmd = {'gofmt -w', 'goimports -w'}, tempfile_postfix = '.tmp'}},
-	javascript = {{cmd = {'prettier --write', 'eslint --fix'}}},
+	javascript = {{cmd = {'yarn prettier --write', 'yarn eslint --fix'}}},
 	markdown = {
-		{cmd = {'prettier --write'}},
+		{cmd = {'yarn prettier --write'}},
 		{cmd = {'black'}, start_pattern = '^```python$', end_pattern = '^```$', target = 'current'},
 	},
 }
-
-vimp.nnoremap({'silent', 'override'}, '<leader>l', ':FormatWrite')

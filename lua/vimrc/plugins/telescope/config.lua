09ce55d5d90d6926ee2474ee1fa0fd5b49115d98
Roee Shapira <roy.shap1@gmail.com>
2021-07-15 12:39:34 +0300

    Removed telescope dap for now

--- lua/vimrc/plugins/telescope/config.lua
+++ lua/vimrc/plugins/telescope/config.lua
@@ -2,11 +2,8 @@ local telescope = require 'telescope'
 local _actions = require 'telescope.actions'
 local trouble = require 'trouble.providers.telescope'
 
-require'session-lens'.setup {shorten_path = true, theme_conf = {border = true}}
-require'telescope'.load_extension 'dap'
-
 telescope.setup {
-	defaults = {
-		mappings = {i = {['<C-t>'] = trouble.open_with_trouble}, n = {['<C-t>'] = trouble.open_with_trouble}},
-	},
+	defaults = {mappings = {i = {['<C-t>'] = trouble.open_with_trouble}, n = {['<C-t>'] = trouble.open_with_trouble}}},
 }
+
+require'session-lens'.setup {shorten_path = true, theme_conf = {border = true}}

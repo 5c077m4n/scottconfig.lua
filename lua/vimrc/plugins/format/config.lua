af454a02a7b45563ee1636fecdd838055dc51545
Roee Shapira <roy.shap1@gmail.com>
2021-07-15 13:15:15 +0300

    Added fmt temp dir

--- lua/vimrc/plugins/format/config.lua
+++ lua/vimrc/plugins/format/config.lua
@@ -1,15 +1,22 @@
 local function prettierFmt()
-	return {exe = 'prettier', args = {'--stdin-filepath', vim.api.nvim_buf_get_name(0)}, stdin = true}
+	return {
+		exe = 'prettier',
+		args = {'--stdin-filepath', vim.api.nvim_buf_get_name(0)},
+		stdin = true,
+		tempfile_dir = '/tmp/fmt-file/',
+	}
 end
 
 require'formatter'.setup {
 	logging = false,
 	filetype = {
-		javascript = {prettierFmt],
+		javascript = {prettierFmt},
 		javascriptreact = {prettierFmt},
 		typescript = {prettierFmt},
 		typescriptreact = {prettierFmt},
-		rust = {function() return {exe = 'rustfmt', args = {'--emit=stdout'}, stdin = true} end},
-		lua = {function() return {exe = 'lua-format', stdin = true} end},
+		rust = {
+			function() return {exe = 'rustfmt', args = {'--emit=stdout'}, stdin = true, tempfile_dir = '/tmp/fmt-file/'} end,
+		},
+		lua = {function() return {exe = 'lua-format', stdin = true, tempfile_dir = '/tmp/fmt-file/'} end},
 	},
 }

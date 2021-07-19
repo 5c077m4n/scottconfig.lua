ec1ec3b405d8b3408dab5cf72d645d8aa49e6abe
Roee Shapira <roy.shap1@gmail.com>
2021-07-19 23:59:36 +0300

    Added all lua files to stylua scan

--- ftplugin/javascript.lua
+++ ftplugin/javascript.lua
@@ -2,5 +2,5 @@ vim.cmd [[packadd nvim-dap]]
 vim.cmd [[packadd nvim-dap-ui]]
 vim.cmd [[packadd DAPInstall.nvim]]
 
-require'dap-install'.config('jsnode_dbg', {})
-require'dapui'.setup()
+require('dap-install').config('jsnode_dbg', {})
+require('dapui').setup()
--- ftplugin/rust.lua
+++ ftplugin/rust.lua
@@ -2,5 +2,5 @@ vim.cmd [[packadd nvim-dap]]
 vim.cmd [[packadd nvim-dap-ui]]
 vim.cmd [[packadd DAPInstall.nvim]]
 
-require'dap-install'.config('ccppr_lldb_dbg', {})
-require'dapui'.setup()
+require('dap-install').config('ccppr_lldb_dbg', {})
+require('dapui').setup()

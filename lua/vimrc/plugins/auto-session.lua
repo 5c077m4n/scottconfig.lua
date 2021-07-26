071a5ba241c024e73da4fbc712691d3388372b12
Roee Shapira <roy.shap1@gmail.com>
2021-07-26 21:02:25 +0300

    Removed unneeded lazyloading

--- lua/vimrc/plugins/auto-session.lua
+++ lua/vimrc/plugins/auto-session.lua
@@ -4,7 +4,4 @@ require('auto-session').setup({
 	auto_session_enabled = true,
 	auto_save_enabled = true,
 	auto_restore_enabled = true,
-	-- Hooks
-	pre_save = { 'NvimTreeClose' },
-	post_restore = { 'NvimTreeOpen' },
 })

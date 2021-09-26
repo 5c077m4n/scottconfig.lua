e5b21b118e4a229bae002c6b5d82926833ee0360
Roee Shapira <ro33.sha@gmail.com>
2021-09-26 12:53:46 +0300

    Removed legacy options from ntree config

--- lua/vimrc/plugins/nvim-tree/config.lua
+++ lua/vimrc/plugins/nvim-tree/config.lua
@@ -5,14 +5,10 @@ g.nvim_tree_disable_default_keybindings = 1
 g.nvim_tree_special_files = { ['README.md'] = 1, ['package.json'] = 1, ['.editorconfig'] = 1 }
 g.nvim_tree_side = 'left'
 g.nvim_tree_width = 40
-g.nvim_tree_auto_open = 1
-g.nvim_tree_auto_close = 0
 g.nvim_tree_quit_on_open = 0
-g.nvim_tree_follow = 1
 g.nvim_tree_indent_markers = 1
 g.nvim_tree_hide_dotfiles = 0
 g.nvim_tree_git_hl = 1
 g.nvim_tree_root_folder_modifier = ':~'
-g.nvim_tree_tab_open = 1
 g.nvim_tree_allow_resize = 0
 g.nvim_tree_add_trailing = 1

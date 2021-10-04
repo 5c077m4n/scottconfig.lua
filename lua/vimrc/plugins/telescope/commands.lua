0bc2aa8f2475c498ac3869a3c44263bb5a114599
Roee Shapira <ro33.sha@gmail.com>
2021-10-04 23:10:20 +0300

    Change global string search command

--- lua/vimrc/plugins/telescope/commands.lua
+++ lua/vimrc/plugins/telescope/commands.lua
@@ -3,6 +3,7 @@ local telescope_builtin = require('telescope.builtin')
 vimp.nnoremap('<leader>fls', telescope_builtin.find_files)
 vimp.nnoremap('<leader>fgs', telescope_builtin.git_files)
 vimp.nnoremap('<leader>fs', telescope_builtin.live_grep)
+vimp.nnoremap('<leader>f#', telescope_builtin.grep_string)
 vimp.nnoremap('<leader>fb', telescope_builtin.buffers)
 vimp.nnoremap('<leader>fm', telescope_builtin.marks)
 vimp.nnoremap('<leader>fo', telescope_builtin.oldfiles)
@@ -10,7 +11,6 @@ vimp.nnoremap('<leader>fc', telescope_builtin.git_commits)
 vimp.nnoremap('<leader>ffc', telescope_builtin.git_bcommits)
 vimp.nnoremap('<leader>ffs', telescope_builtin.current_buffer_fuzzy_find)
 vimp.nnoremap('<leader>ffy', telescope_builtin.lsp_document_symbols)
-vimp.nnoremap('<leader>ffS', telescope_builtin.grep_string)
 vimp.nnoremap('<leader>fft', telescope_builtin.filetypes)
 vimp.nnoremap('<leader>f?', telescope_builtin.commands)
 
@@ -20,7 +20,6 @@ require('which-key').register({
 		f = {
 			name = 'Current file search',
 			c = 'Commit list',
-			s = 'Fuzzy find',
 			t = 'Filetypes',
 			S = 'String under cursor',
 			y = 'Symbol search',
@@ -32,6 +31,7 @@ require('which-key').register({
 		o = 'Old files',
 		c = 'Git commits',
 		m = 'Bookmarks',
+		['#'] = 'Search project for current string',
 		['?'] = 'Commands',
 	},
 })

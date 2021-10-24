0a44861aa87fc36393ce34d5ddf18882ee0d2f77
Roee Shapira <ro33.sha@gmail.com>
2021-10-24 23:25:29 +0300

    Remove already implemented functions in nvim

--- lua/vimrc/globals.lua
+++ lua/vimrc/globals.lua
@@ -3,39 +3,3 @@ function _G.dump(...)
 	print(unpack(objects))
 	return ...
 end
-
-function table.copy(orig)
-	local orig_type = type(orig)
-	local copy
-	if orig_type == 'table' then
-		copy = {}
-		for orig_key, orig_value in pairs(orig) do
-			copy[orig_key] = orig_value
-		end
-	else -- number, string, boolean, etc
-		copy = orig
-	end
-	return copy
-end
-
-function table.deep_copy(orig, copies)
-	copies = copies or {}
-	local orig_type = type(orig)
-	local copy
-
-	if orig_type == 'table' then
-		if copies[orig] then
-			copy = copies[orig]
-		else
-			copy = {}
-			copies[orig] = copy
-			for orig_key, orig_value in next, orig, nil do
-				copy[table.deep_copy(orig_key, copies)] = table.deep_copy(orig_value, copies)
-			end
-			setmetatable(copy, table.deep_copy(getmetatable(orig), copies))
-		end
-	else -- number, string, boolean, etc
-		copy = orig
-	end
-	return copy
-end

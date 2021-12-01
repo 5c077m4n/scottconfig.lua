23a9fa4d3174697e18bb835e04787b7d39b9a1bb
Roee Shapira <ro33.sha@gmail.com>
2021-12-01 23:33:59 +0200

    Update floaterm config

--- lua/vimrc/plugins/terminal/config.lua
+++ lua/vimrc/plugins/terminal/config.lua
@@ -1,6 +1,7 @@
-vim.g.floaterm_autoclose = 1
-vim.g.floaterm_autohide = 2
+local g = vim.g
 
-vim.g.floaterm_width = 0.8
-vim.g.floaterm_height = 0.8
-vim.g.floaterm_position = 'bottomright'
+g.floaterm_width = 0.8
+g.floaterm_height = 0.8
+
+g.floaterm_wintype = 'vsplit'
+g.floaterm_position = 'bottomright'

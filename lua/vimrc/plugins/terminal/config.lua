e7e0ebf18708cd46afe0a94fadda7ba7677599a2
Roee Shapira <ro33.sha@gmail.com>
2021-12-01 23:44:13 +0200

    Revert floaterm config

--- lua/vimrc/plugins/terminal/config.lua
+++ lua/vimrc/plugins/terminal/config.lua
@@ -3,5 +3,5 @@ local g = vim.g
 g.floaterm_width = 0.8
 g.floaterm_height = 0.8
 
-g.floaterm_wintype = 'vsplit'
-g.floaterm_position = 'botright'
+g.floaterm_wintype = 'float'
+g.floaterm_position = 'bottomright'

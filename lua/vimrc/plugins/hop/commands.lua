05755595c7c6c9ce3c00623ff82c53956960f378
Roee Shapira <ro33.sha@gmail.com>
2021-12-12 10:40:51 +0200

    Update hop config

--- lua/vimrc/plugins/hop/commands.lua
+++ lua/vimrc/plugins/hop/commands.lua
@@ -1,4 +1,8 @@
-local hop = require('hop')
 local vimp = require('vimp')
+local hop = require('hop')
+local hop_hint = require('hop.hint')
 
-vimp.nnoremap('f', hop.hint_words)
+vimp.nnoremap('f', function()
+	hop.hint_char1({ direction = hop_hint.HintDirection.AFTER_CURSOR, current_line_only = true })
+end)
+vimp.nnoremap('F', hop.hint_words)

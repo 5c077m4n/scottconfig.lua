dd1a0df78fec7027ccc60161b5e917d071c0d76f
Roee Shapira <roee.shapira@tensorleap.ai>
2021-12-29 23:31:19 +0200

    Disable null-ls gitsigns actions

--- lua/vimrc/plugins/init.lua
+++ lua/vimrc/plugins/init.lua
@@ -196,7 +196,7 @@ local function init_packer()
 							-- misc
 							null_ls.builtins.completion.spell,
 							null_ls.builtins.diagnostics.editorconfig_checker,
-							null_ls.builtins.code_actions.gitsigns,
+							-- null_ls.builtins.code_actions.gitsigns,
 						},
 					})
 					null_ls.disable({

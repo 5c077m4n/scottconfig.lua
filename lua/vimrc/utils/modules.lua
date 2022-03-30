758adb2426e61010102648e3cc515db114c8b922
Roee Shapira <ro33.sha@gmail.com>
2022-03-30 21:54:50 +0300

    Use plenary for vimrc reload

--- lua/vimrc/utils/modules.lua
+++ lua/vimrc/utils/modules.lua
@@ -2,19 +2,9 @@ local utils = require('vimrc.utils')
 
 local M = {}
 
-function M.unload_lua_namespace(prefix)
-	local prefix_with_dot = prefix .. '.'
-
-	for module_name, _ in pairs(package.loaded) do
-		if module_name == prefix or string.match(module_name, '^' .. prefix_with_dot) then
-			package.loaded[module_name] = nil
-		end
-	end
-end
-
 function M.reload_vimrc()
 	require('vimp').unmap_all()
-	M.unload_lua_namespace('vimrc')
+	require('plenary.reload').reload_module('vimrc')
 	require('vimrc')
 	require('packer').compile()
 

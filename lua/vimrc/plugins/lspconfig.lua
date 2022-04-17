af47f77c763a57e63fedc0a9d8e8317649a4f532
Roee Shapira <ro33.sha@gmail.com>
2022-04-17 10:54:00 +0300

    Refactor on_attach keymaps

--- lua/vimrc/plugins/lspconfig.lua
+++ lua/vimrc/plugins/lspconfig.lua
@@ -1,5 +1,4 @@
 local lsp_installer = require('nvim-lsp-installer')
-local lua_dev = require('lua-dev')
 local telescope_builtin = require('telescope.builtin')
 local lsp_status = require('lsp-status')
 local which_key = require('which-key')
@@ -30,33 +29,40 @@ lsp_installer.settings({
 local function on_attach(client, bufnr)
 	local lsp = vim.lsp
 	local diagnostic = vim.diagnostic
+	local create_command = vim.api.nvim_create_user_command
 
 	vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 
 	lsp.handlers['textDocument/hover'] = lsp.with(lsp.handlers.hover, { border = 'single' })
 	lsp.handlers['textDocument/signatureHelp'] = lsp.with(lsp.handlers.signature_help, { border = 'single' })
 
-	local map_opts = { buffer = bufnr }
-	keymap.nnoremap('gd', telescope_builtin.lsp_definitions, map_opts)
-	keymap.nnoremap('gD', telescope_builtin.lsp_dynamic_workspace_symbols, map_opts)
-	keymap.nnoremap('gs', telescope_builtin.lsp_document_symbols, map_opts)
-	keymap.nnoremap('gS', telescope_builtin.lsp_workspace_symbols, map_opts)
-	keymap.nnoremap('K', lsp.buf.hover, map_opts)
-	keymap.nnoremap('<C-x>', lsp.buf.signature_help, map_opts)
-	keymap.nnoremap('gi', telescope_builtin.lsp_implementations, map_opts)
-	keymap.nnoremap('<leader>gD', lsp.buf.type_definition, map_opts)
-	keymap.nnoremap('<leader>rn', lsp.buf.rename, map_opts)
-	keymap.nnoremap('gr', telescope_builtin.lsp_references, map_opts)
+	keymap.nnoremap('gd', telescope_builtin.lsp_definitions, { buffer = bufnr, desc = 'Go to LSP definition' })
+	keymap.nnoremap(
+		'gD',
+		telescope_builtin.lsp_dynamic_workspace_symbols,
+		{ buffer = bufnr, desc = 'Go to declaration' }
+	)
+	keymap.nnoremap('gs', telescope_builtin.lsp_document_symbols, { buffer = bufnr, desc = 'Get document symbols' })
+	keymap.nnoremap('gS', telescope_builtin.lsp_workspace_symbols, { buffer = bufnr, desc = 'Get workspace symbols' })
+	keymap.nnoremap('K', lsp.buf.hover, { buffer = bufnr, desc = 'Show docs in hover' })
+	keymap.nnoremap('<C-s>', lsp.buf.signature_help, { buffer = bufnr, desc = 'Show function signature' })
+	keymap.nnoremap('gi', telescope_builtin.lsp_implementations, { buffer = bufnr, desc = 'Go to implementation' })
+	keymap.nnoremap('<leader>gD', lsp.buf.type_definition, { buffer = bufnr, desc = 'Go to type definition' })
+	keymap.nnoremap('<leader>rn', lsp.buf.rename, { buffer = bufnr, desc = 'LSP rename' })
+	keymap.nnoremap('gr', telescope_builtin.lsp_references, { buffer = bufnr, desc = 'Find references' })
 	keymap.nnoremap('g[', function()
 		diagnostic.goto_prev({ popup_opts = { border = 'single' } })
-	end, map_opts)
+	end, { buffer = bufnr, desc = 'Go to previous diagnostic' })
 	keymap.nnoremap('g]', function()
 		diagnostic.goto_next({ popup_opts = { border = 'single' } })
-	end, map_opts)
-	keymap.nnoremap('<leader>ca', lsp.buf.code_action, map_opts)
-	keymap.vnoremap('<leader>ca', lsp.buf.range_code_action, map_opts)
-	keymap.nnoremap('<leader>l', lsp.buf.formatting, map_opts)
-	keymap.vnoremap('<leader>l', lsp.buf.range_formatting, map_opts)
+	end, { buffer = bufnr, desc = 'Go to next diagnostic' })
+	keymap.nnoremap('<leader>ca', lsp.buf.code_action, { buffer = bufnr, desc = 'Code action' })
+	keymap.vnoremap('<leader>ca', lsp.buf.range_code_action, { buffer = bufnr, desc = 'Code action for range' })
+	keymap.nnoremap('<leader>l', lsp.buf.formatting, { buffer = bufnr, desc = 'Format page' })
+	keymap.vnoremap('<leader>l', lsp.buf.range_formatting, { buffer = bufnr, desc = 'Format range' })
+
+	create_command('Format', lsp.buf.formatting, { desc = 'Format page' })
+	create_command('FormatRange', lsp.buf.range_formatting, { desc = 'Format range' })
 
 	lsp_status.on_attach(client)
 
@@ -96,7 +102,7 @@ local function setup_servers()
 	lsp_installer.on_server_ready(function(server)
 		local opts
 		if server.name == 'lua' or server.name == 'sumneko_lua' then
-			opts = lua_dev.setup({
+			opts = require('lua-dev').setup({
 				lspconfig = {
 					on_attach = on_attach,
 					settings = {
@@ -111,6 +117,7 @@ local function setup_servers()
 		end
 
 		-- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
+		---@diagnostic disable-next-line: undefined-field
 		server:setup(opts)
 		vim.cmd([[do User LspAttachBuffers]])
 	end)
